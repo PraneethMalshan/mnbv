35be84a76648d72400cc0c5d37c3ee4d6db2a53d
PraneethMalshan <praneethmalshan12345@gmail.com>
2025-06-04 22:03:14 +0530

    Added class

---
+++ src/main/java/com/example/demo/controller/Customer.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
