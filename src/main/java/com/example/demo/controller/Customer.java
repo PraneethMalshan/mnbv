7e1de4de8ec7d02ecbd1cafa2c557f7944b92939
PraneethMalshan <praneethmalshan12345@gmail.com>
2025-06-07 01:11:08 +0530

    Added java part

--- src/main/java/com/example/demo/controller/Customer.java
+++ src/main/java/com/example/demo/controller/Customer.java
@@ -6,6 +6,89 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class Customer {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        /*byte min = Byte.MIN_VALUE;
+        byte max = Byte.MAX_VALUE;
+
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);*/
+
+        /*int x =10;
+        double y =20;
+        x+=y;
+        System.out.println(x);*/
+
+        /*int x =10;
+        double y =20;
+        y=x;
+        System.out.println(y);*/
+
+        /*byte b = 10, b1=20, b2=30;
+        b=(byte) (b+b2);
+        System.out.println(b);*/
+
+        /*int total=555;
+        int n = 10;
+        double avg = (double)total/n;
+        System.out.println("The avg is: "+avg);*/
+
+        /*char ch = 'A';
+        System.out.println(ch);
+
+        ch = 66;
+        System.out.println(ch);
+
+        ch = 65;
+        System.out.println(ch);
+        ch = 90;
+        System.out.println(ch);
+
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            System.out.print(letter+" ");
+        }
+        System.out.println();
+        for (int i = 90; i >= 65; i--) {
+            System.out.print((char)i+" ");
+        }
+        System.out.println();
+        for (int i = 65; i <= 90; i++) {
+            System.out.print((char)i+" ");
+        }*/
+
+        /*int x=17%5;
+        System.out.println(x);*/
+
+        /*int x;
+        x=100;
+        System.out.println(x);
+
+        x++;
+        System.out.println(x);
+        ++x;
+        System.out.println(x);
+        ++x;
+        System.out.println(x);
+        x++;
+        System.out.println(x);
+
+        int y;
+        y=100;
+        System.out.println(y);
+        y--;
+        System.out.println(y);
+        --y;
+        System.out.println(y);*/
+
+        int m,n;
+        m=100;
+        System.out.println(m);
+
+        n=++m;
+        System.out.println(n);
+
+
+
+
+
     }
 }
